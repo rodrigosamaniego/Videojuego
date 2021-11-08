f17ef9d647ea3cd0432494cc2bff60600daaeb44
Rodrigo Samaniego <rodrigosamaniegopiza@gmail.com>
2021-11-08 10:17:01 -0600

    project1

---
+++ main.js
@@ -0,0 +1,14 @@
+//Elementos del DOM
+const $canvas = document.querySelector("canvas")
+const ctx = $canvas.getContext('2d')
+
+//Variables globales
+const hero
+const boss
+const obstacle
+const bullets
+//Clases, propiedades y métodos
+//Instancias de las clases
+//Funciones del flujo del juego
+//Funciones de interacción con el usuario
+//Iniciar el juego
