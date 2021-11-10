077b20f7cd02a38b9d4f6621d6ddaf3bf5a0d697
Rodrigo Samaniego <rodrigosamaniegopiza@gmail.com>
2021-11-10 02:53:23 -0600

    segundo

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const $canvas = document.querySelector("canvas")
 const ctx = $canvas.getContext('2d')
 
 //Variables globales
+let intervalId;
 let frames = 0;
+const friction = 0.8;
+const gravity = 0.8;
 const bullets = [];
 const obstacles = [];
 
@@ -44,49 +47,86 @@ class Character extends GameAsset{
         super(x, y, width, height, img);
         this.vy = 0;
         this.vx = 0;
+        this.jumpStrength = 14;
+        // this.jumps = 0;
+		// this.jumping = false;
 }
     draw() {
+        this.vy += gravity;
         this.y += this.vy;
         this.x += this.vx;
+        if (this.y > $canvas.height - this.height) this.y = $canvas.height  -this.height;
+        if (this.x + this.vx > $canvas.width || this.x + this.vx < 0) this.x = 0;
+        if(this.y < this.height) this.y = this.height;
+        
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        
     }
+    
+    jump() {
+		
+        this.vy = -this.jumpStrength;
+    }
+    
     moveLeft(){
-        this.vx-= 2;
+        this.vx-= 3;
+        if(this.vx < -4) this.vx = -3;
     }
+        
     moveRight(){
-        this.vx+= 2;
+        this.vx+= 3;
+        if(this.vx > 4) this.vx = 3;
     }
+    
+    
     stop(){
         this.vx = 0;
     }
+
+    
         
 }
    
+class Enemy extends GameAsset {
+    constructor(x, y, width, height, img){
+        super(x, y, width, height, img);
+        this.vx = 0;
+        this.vy = 0;
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+        
+}
 
 class Obstacle extends GameAsset{
     constructor(x, y, width, height, img) {
         super($canvas.width, y, width, height, img)
     }
     draw(){
-        this.x--
+        this.x-=5;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        if(this.x < 0) this.x = $canvas.width;
     }
 }
 
-
-//class Boss {}
-
-
 //Instancias de las clases
 const boardImage = 
-"/images/background.jpg";
+"/images/background.recortado.enserio.jpg";
 
 const ninjaImage =
 "/images/Genji-OWart.png";
 
-const board = new Board(0, 0, $canvas.width, $canvas.height, boardImage);
-const ninja = new Character(100, 185, 138, 150, ninjaImage);
+const bossImage =
+"/images/MSOGT_Bowser.png";
 
+const obstacleImage =
+"/images/bomb.png";
+
+const board = new Board(0, 0, $canvas.width, $canvas.height, boardImage);
+const ninja = new Character(100, $canvas.height -215, 138, 150, ninjaImage);
+const boss = new Enemy($canvas.width -290, 160, 290, 243, bossImage)
+const bomb = new Obstacle($canvas.width -53, 348, 53, 54, obstacleImage)
 //Funciones del flujo del juego
 function start(){
     setInterval(() => {
@@ -96,12 +136,16 @@ function start(){
 
 
 function update(){
+    frames ++
    clearCanvas();
    board.draw();
    ninja.draw();
+   boss.draw();
+   bomb.draw();
+   
+}
 
     
-}
 
 
 
@@ -109,6 +153,10 @@ function update(){
 
 function clearCanvas () {
     ctx.clearRect(0, 0, $canvas.width, $canvas.height)
+}
+
+function generateObstacles(){
+    // if(frames % 300 === 0) 
 }
 
 //Funciones de interacción con el usuario
@@ -121,7 +169,9 @@ document.onkeydown = (event) => {
         case "ArrowRight":
             ninja.moveRight();
             break;
-
+        case "ArrowUp":
+            ninja.jump();
+            break;
         case "Enter":
             start();
             break;
@@ -138,6 +188,7 @@ document.onkeyup = (event) => {
         case "ArrowRight":
             ninja.stop();
             break;
+        
     
         default:
             break;
